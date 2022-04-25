72ea2c34c97a269fbd105747647c41c721c9a461
Euxen <kelvinhezhang@live.com>
2022-04-25 04:01:51 -0400

    Now we need to find a way to validate the login and send the user to a new View that only shows their own lists.......

---
+++ Controllers/ProfilesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConstruccionDeSoftware_ProyectoFinal.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        // GET: Profile
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
